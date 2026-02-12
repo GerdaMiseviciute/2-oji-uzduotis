fa7de7e7fed9e3463be3bcf6d5219d9d06e16668
GerdaMiseviciute <gerdamis7@gmail.com>
2026-02-12 21:17:41 +0200

    Studentų struktūra

--- main.cpp
+++ main.cpp
@@ -4,8 +4,17 @@
 #include <iomanip>
 using namespace std;
 
+struct Studentas
+{
+    string vardas, pavarde;
+    vector<int> pazymiai;
+    int egzaminas;
+    int galutinis;
+};
+
 int main()
 {
+    vector<Studentas> studentai;
 
     return 0;
 }
